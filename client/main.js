6293763c4b7a1caa0a618a363339e90003edd5ee
Samuel Conteras <sammycontreras7@gmail.com>
2021-11-05 11:00:41 -0600

    first push

---
+++ client/main.js
@@ -0,0 +1,17 @@
+// let complimentButton = document.getElementById("complimentButton").onclick = function () {
+//     axios.get("http://localhost:4000/api/compliment/")
+//         .then(function (response) {
+//           const data = response.data;
+//           alert(data);
+//         });
+//     };
+//     document.getElementById("complimentButton").addEventListener("click", complimentButton)
+
+
+// let movitantionBtn = document.getElementById("motivationBtn").onclick = function () {
+//     axios.get("http://localhost:4000/api/compliment/")
+//     .then(function (response) {
+//       const data = response.data;
+//       alert(data);
+//     });
+// };
